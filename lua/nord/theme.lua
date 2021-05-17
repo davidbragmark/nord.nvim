77549a78a54d53f5af0110662b82dbf4cf488b2e
David Bragmark <david.bragmark@valtech.com>
2021-05-17 16:14:21 +0200

    space over tabs

--- lua/nord/theme.lua
+++ lua/nord/theme.lua
@@ -5,37 +5,37 @@ local theme = {}
 theme.loadSyntax = function ()
     -- Syntax highlight groups
 
-	local syntax = {
-		Type =						{ fg = nord.nord9_gui}, -- int, long, char, etc.
+  local syntax = {
+    Type =      { fg = nord.nord9_gui}, -- int, long, char, etc.
     Identifier =          { fg = nord.nord8_gui },
-		StorageClass =				{ fg = nord.nord9_gui}, -- static, register, volatile, etc.
-		Structure =					{ fg = nord.nord9_gui}, -- struct, union, enum, etc.
-		Constant =					{ fg = nord.nord4_gui}, -- any constant
-		String =					{ fg = nord.nord14_gui, bg = nord.none, style= 'italic' }, -- Any string
-		Character =					{ fg = nord.nord14_gui}, -- any character constant: 'c', '\n'
-		Number =					{ fg = nord.nord15_gui }, -- a number constant: 5
-		Boolean =					{ fg = nord.nord9_gui}, -- a boolean constant: TRUE, false
-		Float =						{ fg = nord.nord15_gui}, -- a floating point constant: 2.3e10
-		Statement =					{ fg = nord.nord9_gui}, -- any statement
-		Label =						{ fg = nord.nord9_gui }, -- case, default, etc.
-		Operator =					{ fg = nord.nord9_gui }, -- sizeof", "+", "*", etc.
-		Exception =					{ fg = nord.nord9_gui}, -- try, catch, throw
-		PreProc =					{ fg = nord.nord9_gui}, -- generic Preprocessor
-		Include =					{ fg = nord.nord9_gui}, -- preprocessor #include
-		Define =					{ fg = nord.nord9_gui}, -- preprocessor #define
-		Macro =						{ fg = nord.cyan }, -- same as Define
-		Typedef =					{ fg = nord.cyan }, -- A typedef
-		PreCondit =					{ fg = nord.pink }, -- preprocessor #if, #else, #endif, etc.
-		Special =					{ fg = nord.nord4_gui}, -- any special symbol
-		SpecialChar =					{ fg = nord.nord13_gui}, -- special character in a constant
-		Tag =						{ fg = nord.nord4_gui}, -- you can use CTRL-] on this
-		Delimiter =					{ fg = nord.nord6_gui }, -- character that needs attention like , or .
-		SpecialComment =				{ fg = nord.nord8_gui}, -- special things inside a comment
-		Debug =						{ fg = nord.red }, -- debugging statements
-		Underlined =					{ fg = nord.link, bg = nord.none, style = 'underline' }, -- text that stands out, HTML links
-		Ignore =					{ fg = nord.disabled }, -- left blank, hidden
-		Error =						{ fg = nord.error, bg = nord.none, style = 'bold,underline' }, -- any erroneous construct
-		Todo =						{ fg = nord.nord13_gui, bg = nord.none, style = 'bold,italic' }, -- anything that needs extra attention; mostly the keywords TODO FIXME and XXX
+    StorageClass =    { fg = nord.nord9_gui}, -- static, register, volatile, etc.
+    Structure =     { fg = nord.nord9_gui}, -- struct, union, enum, etc.
+    Constant =     { fg = nord.nord4_gui}, -- any constant
+    String =     { fg = nord.nord14_gui, bg = nord.none, style= 'italic' }, -- Any string
+    Character =     { fg = nord.nord14_gui}, -- any character constant: 'c', '\n'
+    Number =     { fg = nord.nord15_gui }, -- a number constant: 5
+    Boolean =     { fg = nord.nord9_gui}, -- a boolean constant: TRUE, false
+    Float =      { fg = nord.nord15_gui}, -- a floating point constant: 2.3e10
+    Statement =     { fg = nord.nord9_gui}, -- any statement
+    Label =      { fg = nord.nord9_gui }, -- case, default, etc.
+    Operator =     { fg = nord.nord9_gui }, -- sizeof", "+", "*", etc.
+    Exception =     { fg = nord.nord9_gui}, -- try, catch, throw
+    PreProc =     { fg = nord.nord9_gui}, -- generic Preprocessor
+    Include =     { fg = nord.nord9_gui}, -- preprocessor #include
+    Define =     { fg = nord.nord9_gui}, -- preprocessor #define
+    Macro =      { fg = nord.cyan }, -- same as Define
+    Typedef =     { fg = nord.cyan }, -- A typedef
+    PreCondit =     { fg = nord.pink }, -- preprocessor #if, #else, #endif, etc.
+    Special =     { fg = nord.nord4_gui}, -- any special symbol
+    SpecialChar =     { fg = nord.nord13_gui}, -- special character in a constant
+    Tag =      { fg = nord.nord4_gui}, -- you can use CTRL-] on this
+    Delimiter =     { fg = nord.nord6_gui }, -- character that needs attention like , or .
+    SpecialComment =    { fg = nord.nord8_gui}, -- special things inside a comment
+    Debug =      { fg = nord.red }, -- debugging statements
+    Underlined =     { fg = nord.link, bg = nord.none, style = 'underline' }, -- text that stands out, HTML links
+    Ignore =     { fg = nord.disabled }, -- left blank, hidden
+    Error =      { fg = nord.error, bg = nord.none, style = 'bold,underline' }, -- any erroneous construct
+    Todo =      { fg = nord.nord13_gui, bg = nord.none, style = 'bold,italic' }, -- anything that needs extra attention; mostly the keywords TODO FIXME and XXX
     htmlH1 = { fg = nord.cyan, style = "bold" },
     htmlH2 = { fg = nord.red, style = "bold" },
     htmlH3 = { fg = nord.green, style = "bold" },
@@ -47,15 +47,15 @@ theme.loadSyntax = function ()
     markdownH1Delimiter = { fg = nord.cyan },
     markdownH2Delimiter = { fg = nord.red },
     markdownH3Delimiter = { fg = nord.green },
-	}
+  }
 
-	syntax.Comment =		{fg = nord.nord3_gui_bright}
-	syntax.Conditional =	{fg = nord.nord9_gui} -- normal if, then, else, endif, switch, etc.
-	syntax.Keyword =		{fg = nord.nord9_gui} -- normal for, do, while, etc.
-	syntax.Repeat =			{fg = nord.nord9_gui} -- normal any other keyword
-	syntax.Function =		{fg = nord.nord8_gui}
+  syntax.Comment =  {fg = nord.nord3_gui_bright}
+  syntax.Conditional = {fg = nord.nord9_gui} -- normal if, then, else, endif, switch, etc.
+  syntax.Keyword =  {fg = nord.nord9_gui} -- normal for, do, while, etc.
+  syntax.Repeat =   {fg = nord.nord9_gui} -- normal any other keyword
+  syntax.Function =  {fg = nord.nord8_gui}
 
-    return syntax
+  return syntax
 
 end
 
@@ -63,115 +63,115 @@ end
 theme.loadEditor = function ()
     -- Editor highlight groups
 
-	local editor = {
-		NormalFloat =			{ fg = nord.fg, bg = nord.float }, -- normal text and background color
-		ColorColumn =			{ fg = nord.nord1_gui, bg = nord.active }, --  used for the columns set with 'colorcolumn'
-		Conceal =				{ fg = nord.disabled }, -- placeholder characters substituted for concealed text (see 'conceallevel')
-		Cursor =				{ fg = nord.nord4_gui, bg = nord.none, style = 'reverse' }, -- the character under the cursor
-		CursorIM =				{ fg = nord.cursor, bg = nord.none, style = 'reverse' }, -- like Cursor, but used when in IME mode
-		Directory =				{ fg = nord.blue, bg = nord.none }, -- directory names (and other special names in listings)
-		DiffAdd =				{ fg = nord.green, bg = nord.none, style = 'reverse' }, -- diff mode: Added line
-		DiffChange =			{ fg = nord.orange, bg = nord.none, style = 'reverse' }, --  diff mode: Changed line
-		DiffDelete =			{ fg = nord.red, bg = nord.none, style = 'reverse' }, -- diff mode: Deleted line
-		DiffText =				{ fg = nord.yellow, bg = nord.none, style = 'reverse' }, -- diff mode: Changed text within a changed line
-		EndOfBuffer =			{ fg = nord.disabled },
-		ErrorMsg =				{ fg = nord.none },
-		Folded =				{ fg = nord.disabled, nord.none, style = 'italic' },
-		FoldColumn =			{ fg = nord.blue },
-		IncSearch =				{ fg = nord.highlight, bg = nord.white, style = 'reverse' },
-		LineNr =				{ fg = nord.line_numbers },
-		CursorLineNr =			{ fg = nord.nord8_gui },
-		MatchParen =			{ fg = nord.yellow, bg = nord.none, style = 'bold' },
-		ModeMsg =				{ fg = nord.accent },
-		MoreMsg =				{ fg = nord.accent },
-		NonText =				{ fg = nord.disabled },
-		Pmenu =					{ fg = nord.text, bg = nord.contrast },
-		PmenuSel =				{ fg = nord.accent, bg = nord.active },
-		PmenuSbar =				{ fg = nord.text, bg = nord.contrast },
-		PmenuThumb =			{ fg = nord.fg, bg = nord.accent },
-		Question =				{ fg = nord.green },
-		QuickFixLine =			{ fg = nord.highlight, nord.white, style = 'reverse' },
-		qfLineNr =				{ fg = nord.highlight, nord.white, style = 'reverse' },
-		Search =				{ fg = nord.highlight, bg = nord.white, style = 'reverse' },
-		SpecialKey =			{ fg = nord.purple },
-		SpellBad =				{ fg = nord.red, bg = nord.none, style = 'italic,undercurl' },
-		SpellCap =				{ fg = nord.blue, bg = nord.none, style = 'italic,undercurl' },
-		SpellLocal =			{ fg = nord.cyan, bg = nord.none, style = 'italic,undercurl' },
-		SpellRare =				{ fg = nord.purple, bg = nord.none, style = 'italic,undercurl' },
-		StatusLine =			{ fg = nord.fg, bg = nord.purple },
-		StatusLineNC =  		{ fg = nord.text, bg = nord.disabled },
-		StatusLineTerm =		{ fg = nord.fg, bg = nord.contrast },
-		StatusLineTermNC =		{ fg = nord.text, bg = nord.disabled },
-		TabLineFill =			{ fg = nord.fg },
-		TablineSel =			{ fg = nord.bg, bg = nord.accent },
-		Tabline =				{ fg = nord.fg },
-		Title =					{ fg = nord.green, bg = nord.none, style = 'bold' },
-		Visual =				{ fg = nord.none, bg = nord.selection },
-		VisualNOS =				{ fg = nord.none, bg = nord.selection },
-		WarningMsg =			{ fg = nord.yellow },
-		WildMenu =				{ fg = nord.orange, bg = nord.none, style = 'bold' },
-		CursorColumn =			{ fg = nord.none, bg = nord.active },
-		CursorLine =			{ fg = nord.none, bg = nord.none },
-		ToolbarLine =			{ fg = nord.fg, bg = nord.bg_alt },
-		ToolbarButton =			{ fg = nord.fg, bg = nord.none, style = 'bold' },
-		NormalMode =			{ fg = nord.accent, bg = nord.none, style = 'reverse' },
-		InsertMode =			{ fg = nord.green, bg = nord.none, style = 'reverse' },
-		ReplacelMode =			{ fg = nord.red, bg = nord.none, style = 'reverse' },
-		VisualMode =			{ fg = nord.purple, bg = nord.none, style = 'reverse' },
-		CommandMode =			{ fg = nord.gray, bg = nord.none, style = 'reverse' },
-		Warnings =				{ fg = nord.yellow },
+  local editor = {
+    NormalFloat =   { fg = nord.fg, bg = nord.float }, -- normal text and background color
+    ColorColumn =   { fg = nord.nord1_gui, bg = nord.active }, --  used for the columns set with 'colorcolumn'
+    Conceal =    { fg = nord.disabled }, -- placeholder characters substituted for concealed text (see 'conceallevel')
+    Cursor =    { fg = nord.nord4_gui, bg = nord.none, style = 'reverse' }, -- the character under the cursor
+    CursorIM =    { fg = nord.cursor, bg = nord.none, style = 'reverse' }, -- like Cursor, but used when in IME mode
+    Directory =    { fg = nord.blue, bg = nord.none }, -- directory names (and other special names in listings)
+    DiffAdd =    { fg = nord.green, bg = nord.none, style = 'reverse' }, -- diff mode: Added line
+    DiffChange =   { fg = nord.orange, bg = nord.none, style = 'reverse' }, --  diff mode: Changed line
+    DiffDelete =   { fg = nord.red, bg = nord.none, style = 'reverse' }, -- diff mode: Deleted line
+    DiffText =    { fg = nord.yellow, bg = nord.none, style = 'reverse' }, -- diff mode: Changed text within a changed line
+    EndOfBuffer =   { fg = nord.disabled },
+    ErrorMsg =    { fg = nord.none },
+    Folded =    { fg = nord.disabled, nord.none, style = 'italic' },
+    FoldColumn =   { fg = nord.blue },
+    IncSearch =    { fg = nord.highlight, bg = nord.white, style = 'reverse' },
+    LineNr =    { fg = nord.line_numbers },
+    CursorLineNr =   { fg = nord.nord8_gui },
+    MatchParen =   { fg = nord.yellow, bg = nord.none, style = 'bold' },
+    ModeMsg =    { fg = nord.accent },
+    MoreMsg =    { fg = nord.accent },
+    NonText =    { fg = nord.disabled },
+    Pmenu =     { fg = nord.text, bg = nord.contrast },
+    PmenuSel =    { fg = nord.accent, bg = nord.active },
+    PmenuSbar =    { fg = nord.text, bg = nord.contrast },
+    PmenuThumb =   { fg = nord.fg, bg = nord.accent },
+    Question =    { fg = nord.green },
+    QuickFixLine =   { fg = nord.highlight, nord.white, style = 'reverse' },
+    qfLineNr =    { fg = nord.highlight, nord.white, style = 'reverse' },
+    Search =    { fg = nord.highlight, bg = nord.white, style = 'reverse' },
+    SpecialKey =   { fg = nord.purple },
+    SpellBad =    { fg = nord.red, bg = nord.none, style = 'italic,undercurl' },
+    SpellCap =    { fg = nord.blue, bg = nord.none, style = 'italic,undercurl' },
+    SpellLocal =   { fg = nord.cyan, bg = nord.none, style = 'italic,undercurl' },
+    SpellRare =    { fg = nord.purple, bg = nord.none, style = 'italic,undercurl' },
+    StatusLine =   { fg = nord.fg, bg = nord.purple },
+    StatusLineNC =    { fg = nord.text, bg = nord.disabled },
+    StatusLineTerm =  { fg = nord.fg, bg = nord.contrast },
+    StatusLineTermNC =  { fg = nord.text, bg = nord.disabled },
+    TabLineFill =   { fg = nord.fg },
+    TablineSel =   { fg = nord.bg, bg = nord.accent },
+    Tabline =    { fg = nord.fg },
+    Title =     { fg = nord.green, bg = nord.none, style = 'bold' },
+    Visual =    { fg = nord.none, bg = nord.selection },
+    VisualNOS =    { fg = nord.none, bg = nord.selection },
+    WarningMsg =   { fg = nord.yellow },
+    WildMenu =    { fg = nord.orange, bg = nord.none, style = 'bold' },
+    CursorColumn =   { fg = nord.none, bg = nord.active },
+    CursorLine =   { fg = nord.none, bg = nord.none },
+    ToolbarLine =   { fg = nord.fg, bg = nord.bg_alt },
+    ToolbarButton =   { fg = nord.fg, bg = nord.none, style = 'bold' },
+    NormalMode =   { fg = nord.accent, bg = nord.none, style = 'reverse' },
+    InsertMode =   { fg = nord.green, bg = nord.none, style = 'reverse' },
+    ReplacelMode =   { fg = nord.red, bg = nord.none, style = 'reverse' },
+    VisualMode =   { fg = nord.purple, bg = nord.none, style = 'reverse' },
+    CommandMode =   { fg = nord.gray, bg = nord.none, style = 'reverse' },
+    Warnings =    { fg = nord.yellow },
 
-        healthError =           { fg = nord.error },
-        healthSuccess =         { fg = nord.green },
-        healthWarning =         { fg = nord.yellow },
+    healthError =           { fg = nord.error },
+    healthSuccess =         { fg = nord.green },
+    healthWarning =         { fg = nord.yellow },
 
-        -- dashboard
-        DashboardShortCut =                     { fg = nord.blue },
-        DashboardHeader =                       { fg = nord.purple },
-        DashboardCenter =                       { fg = nord.cyan },
-        DashboardFooter =                       { fg = nord.green, style = "italic" },
+    -- dashboard
+    DashboardShortCut =                     { fg = nord.blue },
+    DashboardHeader =                       { fg = nord.purple },
+    DashboardCenter =                       { fg = nord.cyan },
+    DashboardFooter =                       { fg = nord.green, style = "italic" },
 
-	}
+ }
 
     -- Options:
 
     --Set transparent background
-    if vim.g.nord_disable_background == true then
-		editor.Normal =				{ fg = nord.fg, bg = nord.none } -- normal text and background color
-		editor.SignColumn =			{ fg = nord.fg, bg = nord.none }
-    else
-		editor.Normal =				{ fg = nord.fg, bg = nord.bg } -- normal text and background color
-		editor.SignColumn =			{ fg = nord.fg, bg = nord.bg }
-    end
+  if vim.g.nord_disable_background == true then
+    editor.Normal =    { fg = nord.fg, bg = nord.none } -- normal text and background color
+    editor.SignColumn =   { fg = nord.fg, bg = nord.none }
+  else
+    editor.Normal =    { fg = nord.fg, bg = nord.bg } -- normal text and background color
+    editor.SignColumn =   { fg = nord.fg, bg = nord.bg }
+  end
 
     -- Remove window split borders
-    if vim.g.nord_borders == true then
-		editor.VertSplit =				{ fg = nord.border }
-    else
-		editor.VertSplit =				{ fg = nord.bg }
-    end
+  if vim.g.nord_borders == true then
+    editor.VertSplit =    { fg = nord.border }
+  else
+    editor.VertSplit =    { fg = nord.bg }
+  end
 
-    return editor
+  return editor
 end
 
 theme.loadTerminal = function ()
 
-	vim.g.terminal_color_0 = nord.black
-	vim.g.terminal_color_1 = nord.red
-	vim.g.terminal_color_2 = nord.green
-	vim.g.terminal_color_3 = nord.yellow
-	vim.g.terminal_color_4 = nord.blue
-	vim.g.terminal_color_5 = nord.purple
-	vim.g.terminal_color_6 = nord.cyan
-	vim.g.terminal_color_7 = nord.white
-	vim.g.terminal_color_8 = nord.gray
-	vim.g.terminal_color_9 = nord.red
-	vim.g.terminal_color_10 = nord.green
-	vim.g.terminal_color_11 = nord.yellow
-	vim.g.terminal_color_12 = nord.blue
-	vim.g.terminal_color_13 = nord.purple
-	vim.g.terminal_color_14 = nord.cyan
-	vim.g.terminal_color_15 = nord.white
+  vim.g.terminal_color_0 = nord.black
+  vim.g.terminal_color_1 = nord.red
+  vim.g.terminal_color_2 = nord.green
+  vim.g.terminal_color_3 = nord.yellow
+  vim.g.terminal_color_4 = nord.blue
+  vim.g.terminal_color_5 = nord.purple
+  vim.g.terminal_color_6 = nord.cyan
+  vim.g.terminal_color_7 = nord.white
+  vim.g.terminal_color_8 = nord.gray
+  vim.g.terminal_color_9 = nord.red
+  vim.g.terminal_color_10 = nord.green
+  vim.g.terminal_color_11 = nord.yellow
+  vim.g.terminal_color_12 = nord.blue
+  vim.g.terminal_color_13 = nord.purple
+  vim.g.terminal_color_14 = nord.cyan
+  vim.g.terminal_color_15 = nord.white
 
 end
 
@@ -187,7 +187,7 @@ theme.loadTreeSitter = function ()
         TSNumber =                  { fg = nord.nord15_gui },    -- For all number
         TSString =                  { fg = nord.nord14_gui, style = 'italic' },    -- For strings.
 
-	--TSAnnotation =              { fg = nord.red },    -- For C++/Dart attributes, annotations that can be attached to the code to denote some kind of meta information.
+ --TSAnnotation =              { fg = nord.red },    -- For C++/Dart attributes, annotations that can be attached to the code to denote some kind of meta information.
         TSAttribute =               { fg = nord.yellow },    -- (unstable) TODO: docs
         TSBoolean=                  { fg = nord.nord9_gui },    -- For booleans.
         --TSCharacter=                { fg = nord.orange },    -- For characters.
